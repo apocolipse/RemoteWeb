b3358755f3d47c6940b50311b4b3adcc1852b28e
Chris Simpson <apocolipse@gmail.com>
2019-05-16 20:11:36 -0400

    Update platforms

--- Package.swift
+++ Package.swift
@@ -5,9 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "RemoteWeb",
+    platforms: [ .macOS(.v10_12) ],
     dependencies: [
         .package(url: "https://github.com/apocolipse/swifter", from: "1.4.7"),
-        .package(url: "git@github.com:apocolipse/SwiftLIRC.git", from: "0.1.3")
+        .package(url: "git@github.com:apocolipse/SwiftLIRC.git", from: "0.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
