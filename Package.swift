e137e654d3fd500d07a992ddedb10834268c476d
apocolipse <apocolipse@gmail.com>
2019-05-19 15:16:17 -0400

    Fix send count

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [ .macOS(.v10_12) ],
     dependencies: [
         .package(url: "https://github.com/apocolipse/swifter", from: "1.4.7"),
-        .package(url: "git@github.com:apocolipse/SwiftLIRC.git", from: "0.2.2")
+        .package(url: "git@github.com:apocolipse/SwiftLIRC.git", from: "0.2.5")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
