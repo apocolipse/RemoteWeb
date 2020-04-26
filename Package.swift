82be03dc73be193800f3820783c4ae09a3ced549
Chris Simpson <apocolipse@Chriss-Mac-mini.verizon.net>
2020-04-26 19:44:00 -0400

    Bump SwiftLIRC version

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [ .macOS(.v10_12) ],
     dependencies: [
         .package(url: "https://github.com/apocolipse/swifter", from: "1.4.7"),
-        .package(url: "git@github.com:apocolipse/SwiftLIRC.git", from: "0.2.8")
+        .package(url: "git@github.com:apocolipse/SwiftLIRC.git", from: "0.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
